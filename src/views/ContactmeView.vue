11d1304bb5dd272532713972865a37e106ec137c
wilmerx5 <wilmercampos2004@gmail.com>
2024-10-11 23:34:08 -0500

    dark mode

--- src/views/ContactmeView.vue
+++ src/views/ContactmeView.vue
@@ -15,27 +15,31 @@ import WhatsIcon from '@/components/Icons/WhatsIcon.vue';
 
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-4">
             <!-- Email -->
-            <a href="mailto:wilmercampos2004@gmail.com" class="flex items-center p-4 bg-red-600 text-white rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
+            <a href="mailto:wilmercampos2004@gmail.com"
+                class="flex flex-col items-center p-4 bg-red-600 text-white rounded-lg shadow-md hover:shadow-xl transition-all duration-300 transform hover:scale-105">
                 <GmailIcon class="h-8 w-8 mr-2" />
-                <span>wilmercampos2004@gmail.com</span>
+                <span class="mt-2">wilmercampos2004@gmail.com</span>
             </a>
 
             <!-- LinkedIn -->
-            <a href="https://www.linkedin.com/in/wilmer-campos-arevalo-7a129a273/" target="_blank" class="flex items-center p-4 bg-blue-700 text-white rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
+            <a href="https://www.linkedin.com/in/wilmer-campos-arevalo-7a129a273/" target="_blank"
+                class="flex  flex-col items-center p-4 bg-blue-700 text-white rounded-lg shadow-md hover:shadow-xl transition-all duration-300 hover:scale-105">
                 <LinkedinIcon class="h-8 w-8 mr-2" />
-                <span>LinkedIn Profile</span>
+                <span class="mt-2">LinkedIn Profile</span>
             </a>
 
             <!-- GitHub -->
-            <a href="https://github.com/wilmerx5" target="_blank" class="flex items-center p-4 bg-gray-800 text-white rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
+            <a href="https://github.com/wilmerx5" target="_blank"
+                class="flex flex-col  items-center p-4 bg-gray-800 text-white rounded-lg shadow-md hover:shadow-xl transition-all duration-300 hover:scale-105">
                 <GitHubIcon class="h-8 w-8 mr-2" />
-                <span>@Wilmerx5</span>
+                <span class="mt-2">@Wilmerx5</span>
             </a>
 
             <!-- WhatsApp -->
-            <a href="https://wa.me/573124547085" target="_blank" class="flex items-center p-4 bg-green-500 text-white rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
+            <a href="https://wa.me/573124547085" target="_blank"
+                class="flex  flex-col items-center p-4 bg-green-500 text-white rounded-lg shadow-md hover:shadow-xl transition-all duration-300 hover:scale-105">
                 <WhatsIcon class="h-8 w-8 mr-2" />
-                <span>+57 3124547085</span>
+                <span class="mt-2">+57 3124547085</span>
             </a>
         </div>
 
