422ff09a92663e3842f5ec0f0906bbf93706da19
wilmerx5 <wilmercampos2004@gmail.com>
2024-10-11 23:49:28 -0500

    working

--- src/components/ProjectTitle.vue
+++ src/components/ProjectTitle.vue
@@ -29,14 +29,14 @@ const handleMouseLeave = () => {
 </script>
 
 <template>
-    <div @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave" class="group transition-all duration-300 p-3 border border-gray-300 rounded-md shadow-sm hover:shadow-md transform hover:-translate-y-1">
-      <a :href="url" target="_blank" rel="noopener noreferrer">
+  <a :href="url" target="_blank" rel="noopener noreferrer">
+        <div @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave" class="group transition-all duration-300 p-3 border border-gray-300 rounded-md shadow-sm hover:shadow-md transform hover:-translate-y-1">
         <h3 class="text-gray-800 dark:text-gray-300 text-xl font-semibold group-hover:text-teal-500 transition-colors duration-300">
           <slot></slot>
         </h3>
         <div class="mt-1 h-1 w-0 group-hover:w-full bg-teal-500 transition-all duration-300"></div>
+      </div>
       </a>
-    </div>
   </template>
   
   <style scoped>
